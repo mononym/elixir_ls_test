aa130dc49b70d0c861e15ad9c92e9080d4c06330
Chris Hicks <chris.hicks@thatgamecompany.com>
2025-07-21 12:43:18 -0700

    change formatter

--- apps/elixir_ls_test_web/.formatter.exs
+++ apps/elixir_ls_test_web/.formatter.exs
@@ -1,5 +1,9 @@
 [
   import_deps: [:phoenix],
   plugins: [Phoenix.LiveView.HTMLFormatter],
-  inputs: ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"]
+  inputs:
+    Enum.flat_map(
+      ["*.{heex,ex,exs}", "{config,lib,test}/**/*.{heex,ex,exs}"],
+      &Path.wildcard(&1, match_dot: true)
+    ) -- ["lib/elixir_ls_test_web/router.ex"]
 ]
